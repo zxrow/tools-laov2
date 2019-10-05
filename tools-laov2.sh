03a35219c052be84e54a63b214c0a14904f87a50
Hacketindonedia1 <49530313+Hacketindonedia1@users.noreply.github.com>
2019-10-05 23:32:08 +0700

    New Upload

---
+++ tools-laov2.sh
@@ -0,0 +1,512 @@
+clear
+echo
+echo
+clear
+echo
+python2 Password.py
+echo
+clear
+
+blue='\e[0;34'
+cyan='\e[0;36m'
+green='\e[0;34m'
+okegreen='\033[92m'
+lightgreen='\e[1;32m'
+white='\e[1;37m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+
+echo -e $red"\033[1;97m█████████"
+echo -e $red"\033[1;97m█▄█████▄█      \033[1;96m●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●"
+echo -e $red"\033[1;97m█\033[1;91m▼▼▼▼▼ \033[1;97m- _ --_--\033[1;92m"
+echo -e $red"\033[1;97m█ \033[1;97m \033[1;97m_-_-- -_ --__\033[1;92m"
+echo -e $red"\033[1;97m█\033[1;91m▲▲▲▲▲\033[1;97m--  - _ --\033[1;92m \033[1;93mTools-laov2"
+echo -e $red"\033[1;97m█████████      \033[1;96m«----------✧----------»"
+echo -e $red"\033[1;97m ██ ██"
+echo -e $red"\033[1;97m╔════════════════════════════════════════╗"
+echo -e $red"\033[1;97m║\033[1;93m* \033[1;97mAuthor  \033[1;91m: \033[1;96mLao'Neis agung & rama ardiyansyah \033[1;97m                ║"
+echo -e $red"\033[1;97m║\033[1;93m* \033[1;97mSupport \033[1;91m: \033[1;96mBayyu\033[1;97m[\033[1;96m\033[1;97m] \033[1;97m |\033[1;96mLao'Neis agung\033[1;97m|\033[1;96mBayyu \033[1;97m║"
+echo -e $red"\033[1;97m║\033[1;93m* \033[1;97mGitHub  \033[1;91m: \033[1;92m\033[4mhttps://github.com/Hacketindonedia1\033[0m \033[1;97m║"
+echo -e $red"\033[1;97m╚════════════════════════════════════════╝"
+echo
+sleep 1
+echo -e $red"    [ 01 ]> ${white} tools-darkv1 [√] "
+echo -e $red"    [ 02 ]> ${white} tools-v9 [√] "
+echo -e $red"    [ 03 ]> ${white} tools-v10 [√] "
+echo -e $red"    [ 04 ]> ${white} phisingv2 [√] "
+echo -e $red"    [ 05 ]> ${white} VCB-DDOS[√]"
+echo -e $red"    [ 06 ]> ${white} fb-crack5 [√] "
+echo -e $red"    [ 07 ]> ${white} dark-vpro [√] "
+echo -e $red"    [ 08 ]> ${white} naiTools [√] "
+echo -e $red"    [ 09 ]> ${white} callv2 [√] "
+echo -e $red"    [ 10 ]> ${white} SPSDEFACE [√]"
+echo -e $red"    [ 11 ]> ${white} Spammer Call [√] "
+echo -e $red"    [ 12 ]> ${white} Call New1 [√] "
+echo -e $red"    [ 13 ]> ${white} SpamV3 [√] "
+echo -e $red"    [ 14 ]> ${white} Call-Spam [√] "
+echo -e $red"    [ 15 ]> ${white} toolsby-Yoga-Here [√] "
+echo -e $red"    [ 16 ]> ${white} Dark-Call [√] "
+echo -e $red"    [ 17 ]> ${white} Hacker11 [√] "
+echo -e $red"    [ 18 ]> ${white} Diamond [√] "
+echo -e $red"    [ 19 ]> ${white} Pantek-C2 [√] "
+echo -e $red"    [ 20 ]> ${white} pro2611 [√] "
+echo -e $red"    [ 21 ]> ${white} VIRTEX [√] "
+echo -e $red"    [ 22 ]> ${white} santri fbguard [√] "
+echo -e $red"    [ 23 ]> ${white} unfired [√] "
+echo -e $red"    [ 24 ]> ${white} Lacak IP [√] "
+echo -e $red"    [ 25 ]> ${white} Spam [√] "
+echo -e $red"    [ 26 ]> ${white} dark4 [√] "
+echo -e $red"    [ 27 ]> ${white} reggae [√] "
+echo -e $red"    [ 28 ]> ${white} Mandalan Xtream [√] "
+echo -e $red"    [ 29 ]> ${white} santet online [√] "
+echo -e $red"    [ 30 ]> ${white} jadwal sholat [√] "
+echo -e $red"    [ 31 ]> ${white} tools5 [√] "
+echo -e $red"    [ 32 ]> ${white} exit"
+echo
+read -p "[root@${usnm}]> " slc;
+
+if [ $slc = 1 ] || [ $slc = 01 ]
+then
+clear
+echo -e $green"Menginstall tools-darkv1 "
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/Hacketindonedia1/tools-darkv1
+clear
+echo -e $green"Berhasil Menginstall "
+cd tools-darkv1
+sh tools-darkv1.sh
+fi
+
+if [ $slc = 2  ] || [ $slc = 02 ]
+then
+clear
+echo -e $green"Menginstall tools-v9 "
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/Hacketindonedia1/toolsv9
+clear
+echo -e $green"Berhasil Menginstall "
+cd toolsv9
+sh toolsv9.sh
+fi
+
+if [ $slc = 3 ] || [ $slc = 03 ]
+then
+clear
+echo -e $green"Menginstall tools-v10 "
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/Hacketindonedia1/tools-v10
+clear
+echo -e $green"Berhasil Menginstall "
+cd tools-v10
+sh tools-v10.sh
+fi
+
+if [ $slc = 4 ] || [ $slc = 04 ]
+then
+clear
+echo -e $green"Menginstall phisingv2 "
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/Hacketindonedia1/phisingv2
+clear
+echo -e $green"Berhasil Menginstall "
+cd phisingv2
+sh phisingv2.sh
+fi
+
+if [ $slc = 5 ] || [ $slc = 05 ]
+then
+clear
+echo -e $green"Menginstall VCB.DDOS "
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/Hacketindonedia1/VCB.DDOS
+clear
+echo -e $green"Berhasil Menginstall "
+cd VCB.DDOS
+sh VCB.DDOS.sh
+fi
+
+if [ $slc = 6 ] || [ $slc = 06 ]
+then
+clear
+echo -e $green"Menginstall fb-crack5 "
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/Hacketindonedia1/fbcrack5
+clear
+echo -e $green"Berhasil Menginstall "
+cd fb-crack5
+python2 fbcrack5.py
+fi
+
+if [ $slc = 7 ] || [ $slc = 07 ]
+then
+clear
+echo -e $green"Menginstall dark-vpro "
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/Hacketindonedia1/dark-vpro
+clear
+echo -e $green"Berhasil Menginstall "
+cd dark-vpro
+python2 dark-vpro.py
+fi
+
+if [ $slc = 8 ] || [ $slc = 08 ]
+then
+clear
+echo -e $green"Menginstall naiTools "
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/Hacketindonedia1/naiTools
+clear
+echo -e $green"Berhasil Menginstall "
+cd naiTools
+bash naiTools.sh
+fi
+
+if [ $slc = 9 ] || [ $slc = 09 ]
+then
+clear
+echo -e $green"Menginstall callv2 "
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/Hacketindonedia1/callv2
+clear
+echo -e $green"Berhasil Menginstall "
+cd callv2
+python callv2.py
+fi
+
+if [ $slc = 10 ] || [ $slc = 10 ]
+then
+clear
+echo -e $green"Menginstall SPSDEFACE "
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/Hacketindonedia1/SPSDEFACE
+clear
+echo -e $green"Berhasil Menginstall "
+cd SPSDEFACE
+sh SPSDEFACE.sh
+fi
+
+if [ $slc = 11 ] || [ $slc = 11 ]
+then
+clear
+echo -e $green"Menginstall Spammer-Call"
+clear
+sleep 1
+pkg install git
+pkg install php
+git clone https://github.com/Hacketindonedia1/Spammer-Call
+clear
+echo -e $green"Berhasil Menginstall "
+cd Spammer-Call
+php call.php
+fi
+
+if [ $slc = 12 ] || [ $slc = 12 ]
+then
+clear
+echo -e $gree"Menginstall Call-New1 "
+clear
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/Hacketindonedia1/Call-New1
+clear
+echo -e $green"Berhasil Menginstall "
+cd Call-New1
+sh Call.sh
+fi
+
+
+if [ $slc = 13 ] || [ $slc = 13 ]
+then
+clear
+echo -e $green"Menginstall Spam-V3 "
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/Hacketindonedia1/Spam-V3
+clear
+echo -e $green"Berhasil Menginstall "
+cd SpamV3
+sh new.sh
+fi
+
+if [ $slc = 14 ] || [ $slc = 14 ]
+then
+clear
+echo -e $green"Meninstall Call-Spam "
+sleep 1
+pkg install git
+pkg install php
+git clone https://github.com/Hacketindonedia1/CALL-SPAM
+clear
+echo -e $green"Berhasil Menginstall "
+cd CALL-SPAM
+php CALLSPAM.php
+fi
+
+if [ $sc = 15 ] || [ $slc = 15 ]
+then
+clear
+echo -e $green"Menginstall Toolsby-Yoga-Here "
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/wira2611/kumpulantools
+clear
+echo -e $green"Berhasil Menginstall "
+cd kumpulantools
+sh Wa.sh
+fi
+
+if [ $slc = 16 ] || [ $slc = 16 ]
+then
+clear
+echo -e $green"Menginstall DarkCall "
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/wira2611/Dark-Call
+cd Dark-Call
+clear
+echo -e $green"Berhasil Menginstall "
+cd Dark-Call
+python2 Github.py
+fi
+
+if [ $slc = 17 ] || [ $slc = 17 ]
+then
+clear
+echo -e $green"Menginstall Hacker11 "
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/wira2611/Hacker11
+clear
+echo -e $green"Berhasil Menginstall "
+cd Hacker11
+sh hacker26.sh
+fi
+
+if [ $slc = 18 ] || [ $slc = 18 ]
+then
+clear
+echo -e $green"Menginstall diamond "
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/wira2611/diamon
+clear
+echo -e $green"Berhasil Menginstall "
+cd diamon
+python2 diamon-1.py
+fi
+
+if [ $slc = 19  ] || [ $slc = 19 ]
+then
+clear
+echo -e $green"Menginstall pantek-C2 "
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/wira2611/pantek-C2
+clear
+echo -e $green"Berhasil Menginstall "
+cd pantek-C2
+sh cobak.sh
+fi
+
+if [ $slc = 20  ] || [ $slc = 20 ]
+then
+clear
+echo -e $green"Menginstall pro2611 "
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/wira2611/Pro2611
+clear
+echo -e $green"Berhasil Menginstall "
+cd Pro2611
+python2 Pro.py
+fi
+
+if [ $slc = 21 ] || [ $slc = 21 ]
+then
+clear
+echo -e $green"Menginstall VIRTEX "
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/muhammadfathul/VIRTEX
+clear
+echo -e $green"Berhasil Menginstall "
+cd VIRTEX
+sh virtex.sh
+fi
+
+if [ $slc = 22 ] || [ $slc = 22 ]
+then
+clear
+echo -e $green"Menginstall santri fbguard "
+sleep 1
+pkg install git
+pkg install php
+git clone https://github.com/SantriCyber/santri-fbguard
+clear
+echo -e $green"Berhasil Menginstall "
+cd santri-fbguard
+php santri-fbguard.php
+fi
+
+if [ $slc = 23 ] || [ = $slc = 23 ]
+then
+clear
+echo -e $green"Menginstal unfired "
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/Unikers71/unfriend
+clear
+echo -e $green"Berhasil Menginstall "
+cd unfriend
+python2 unfriend.py
+fi
+
+if [ $slc = 24 ] || [ $slc = 24 ]
+then
+clear
+echo -e $green"Menginstall Lacak ip "
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/unikers71/lacakIP
+clear
+echo -e $green"Berhasil Menginstall "
+cd lacakIP
+python2 IP.py
+fi
+
+if [ $slc = 25 ] || [ $slc = 25 ]
+then
+clear
+echo -e $green"Menginstall Spam "
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/unikers71/spammer
+clear
+echo -e $green"Berhasil Menginstall "
+cd spammer
+python2 spam.py
+fi
+
+if [ $slc = 26 ] || [ $slc = 26 ]
+then
+clear
+echo -e $green"Meninstall Dark4 "
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/cyber2611/Dark4
+clear
+echo -e $green"Berhasil Menginstall "
+cd Dark4
+python2 Dark4.py
+fi
+
+if [ $slc = 27 ] || [ $slc = 27 ]
+then
+clear
+echo -e $green"Menginstall Reggae "
+sleep 1
+pkg install git
+pkg install python2
+git clonehttps://github.com/cyber2611/reggae
+clear
+echo -e $green"Berhasil Menginstall "
+cd reggae
+python2 reggae.py
+fi
+
+if [ $slc = 28 ] || [ $slc = 28 ]
+then
+clear
+echo -e $green"menginstall Mandalan Xtream "
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/cyber2611/MandalanXtream
+clear
+echo -e $green"Berhasil Menginstall "
+cd MandalanXtream
+python2 Mandalan.py
+fi
+
+if [ $slc = 29 ] || [ $slc = 29 ]
+then
+clear
+echo -e $green"menginstall santet online "
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/cyber2611/santet-online
+clear
+echo -e $green"Berhasil Menginstall "
+cd santet-online
+python2 santet.py
+fi
+
+if [ $slc = 30 ] || [ $slc = 30 ]
+then
+clear
+echo -e $green"Menginstall Jadwalshalat "
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/cyber2611/jadwalshalat
+clear
+echo -e $green"Berhasil Menginstall "
+cd jadwalshalat
+python2 jadwal.py
+fi
+
+if [ $slc = 31 ] || [ $slc = 31 ]
+then
+clear
+echo -e $green"Menginstall Tools5 "
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/cyber2611/tools5
+clear
+echo -e $green"Berhasil Menginstall "
+cd tools5
+sh hore.sh
+fi
+
+
+if [ $slc = exit ]
+then
+clear
+echo -e $red"Exiting ..."
+exit
+fi
